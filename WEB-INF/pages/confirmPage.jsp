a05ea4c133e21d8e863a0c5fe793af5f0188b971
寺澤徳隆 <mosqey1889@gmail.com>
2024-09-01 07:34:53 +0900

    確認ページのサーブレットとJSP作成

---
+++ WEB-INF/pages/confirmPage.jsp
@@ -0,0 +1,36 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charaset="UTF-8"></meta>
+		<title>問い合わせフォーム</title>
+	</head>
+
+	<body>
+	    <h2>入力内容をご確認ください。</h2>
+        <p>問題なければ「確定」、修正する場合は「キャンセル」をクリックしてください。</p>
+	    <table border="1">
+	    	<tr>
+	    		<th>項目</th>
+	    		<th>入力内容</th>
+	    	</tr>
+	    	<tr>
+	    		<td>お名前</td>
+	    		<!-- お名前を取得 -->
+	    		<td>${name}</td>
+	    	</tr>
+	    	<tr>
+	    		<td>メールアドレス</td>
+	    		<!-- メールアドレスを取得 -->
+	    		<td>${email}</td>
+	    	</tr>
+	    	<tr>
+	    		<td>お問い合わせ内容</td>
+	    		<td>${message}</td>
+	    	</tr>
+	    </table>
+	    <button onclick="location.href='<%= request.getContextPath() %>/complete';">確定</button>
+	    <button onclick="history.back">キャンセル</button>
+	</body>
+</html>
--- WEB-INF/src/forms/ConfirmServlet.java
+++ WEB-INF/src/forms/ConfirmServlet.java
@@ -1,7 +1,36 @@
 package forms;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class ConfirmServlet extends HttpServlet {
+	
+	// POSTメソッドのリクエスト時に実行されるメソッド
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// リクエスト・レスポンスの設定
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		
+		// JSPからのリクエストデータ取得
+		String name = req.getParameter("name");
+		String email = req.getParameter("email");
+		String message = req.getParameter("message");
+		
+		// リクエストスコープにデータ保存
+		req.setAttribute("name", name);
+		req.setAttribute("email", email);
+		req.setAttribute("message", message);
+		
+		// フォワードによる画面遷移
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/pages/confirmPage.jsp");
+		dispatcher.forward(req, resp);
+	}
 
 }
